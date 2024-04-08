bd6b15640cb4cc8dcd876dfb551eb14b8c9dacc4
sekihan02 <sekihan0291@gmail.com>
2024-04-09 02:18:18 +0900

    add summary

--- react_base/docker_stop.bat
+++ react_base/docker_stop.bat
@@ -1,3 +1,3 @@
 @REM dockerを止める時
-docker stop flask_react_app
-docker rm flask_react_app
+docker stop react-app
+docker rm react-app
---
+++ react_env/react_learning_env__ver18/docker_stop.bat
@@ -0,0 +1,3 @@
+@REM dockerを止める時
+docker stop react-app_env
+docker rm react-app_env
